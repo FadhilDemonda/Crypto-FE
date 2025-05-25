33845bd82f120330857bf05a98974ffa6ce4f4fe
ARiP001 <randomguydx@gmail.com>
2025-05-25 22:38:38 +0700

    token change

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+const API_KEY = 'CG-cR6sjp6AaqrbMFwh87A5v6gx';
+
 const axiosInstance = axios.create({
   baseURL: 'https://crypto-backend-21569344527.us-central1.run.app', // sesuaikan backend lokalmu
   withCredentials: true, // ini wajib supaya cookie dikirim & diterima
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'x-cg-demo-api-key': API_KEY
   }
 });
 
